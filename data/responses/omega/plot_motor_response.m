f61c18fbff2c8c62538cbc9c0d653d162c1c1900
risherlock <risherlock221b@gmail.com>
2024-01-26 19:58:53 +0100

    add motor omega response data

--- data/responses/omega/plot_motor_response.m
+++ data/responses/omega/plot_motor_response.m
@@ -4,16 +4,23 @@ clc
 clear
 close all
 
+dt = 0.025; % Sample time, s
+
 % Import and parse csv file
-data = csvread("response.csv");
+data = csvread("202401261954.csv");
 sp = data(:,1);
-response = data(:,1);
+response = data(:,2);
+
+% Generate time vector
+t = (0:length(sp)-1) * dt;
 
 % Visualization
 figure;
-plot(sp, 'LineWidth', 2); hold on;
-plot(response, 'LineWidth', 2);
-xlim([0, length(data)]);
+plot(t, sp, 'LineWidth', 2); hold on;
+plot(t, response, 'LineWidth', 2);
+xlim([0, max(t)]);
 legend('set point', 'response');
-title('Motor angular velocity [rad/s] response');
+title('PID control of motor angular velocity [rad/s]');
+ylabel('Motor \omega, rad/s');
+xlabel('Time, s');
 grid on;
