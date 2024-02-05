8fa720bc3b75d186b20ec1fab5e328a2434be517
risherlock <risherlock221b@gmail.com>
2024-02-05 13:26:38 +0100

    limit switch succeful test; satellite omega control ok

--- codes/grabsat/threads/arm.cpp
+++ codes/grabsat/threads/arm.cpp
@@ -13,20 +13,21 @@ void ArmThread::init()
 {
   magnet_switch.init(false, 1, 0);
   balance_switch.init(false, 1, 0);
-  balance_switch.config(false, 1);
-  magnet_switch.config(false, 1);
+
+  balance_switch.config(GPIO_CFG_PULLUP_ENABLE, 1);
+  magnet_switch.config(GPIO_CFG_PULLUP_ENABLE, 1);
 }
 
 void ArmThread::run()
 {
   while (1)
   {
-    // if (stop_flag)
-    // {
-    //   magnet_arm.run(0);
-    //   balance_arm.run(0);
-    //   suspendCallerUntil(END_OF_TIME);
-    // }
+    if (stop_flag)
+    {
+      magnet_arm.run(0);
+      balance_arm.run(0);
+      suspendCallerUntil(END_OF_TIME);
+    }
 
     int dir = 0;
     if (dir_flag) // extend
@@ -38,30 +39,32 @@ void ArmThread::run()
       dir = 1;
     }
 
-  if(!magnet_switch.readPins())
-  {
-    PRINTF("MAGNET PRESSED!\n");
-    magnet_arm.run(0);
-  }
-  else // Magnet switch not pressed
-  {
-    PRINTF("MAGNET NOT PRESSED!\n");
     magnet_arm.run(dir * SERVO_ARM_SPEED);
-  }
-
-  if(!balance_switch.readPins())
-  {
-    PRINTF("BALANCE PRESSED!\n");
-    balance_arm.run(0);
-  }
-  else // Balance switch not pressed
-  {
-    PRINTF("BALANCE NOT PRESSED!\n");
     balance_arm.run(dir * SERVO_ARM_SPEED);
-  }
 
     stop_flag = true; // Stop after one execution
-    suspendCallerUntil(NOW() + 2000 * MILLISECONDS);
+    suspendCallerUntil(NOW() + period * MILLISECONDS);
+  }
+}
+
+void ArmThread::check_limits()
+{
+  // Turn off magnet arm
+  if (magnet_switch.readPins())
+  {
+    magnet_arm.run(0);
+  }
+
+  // Turn off balance arm
+  if (balance_switch.readPins())
+  {
+    balance_arm.run(0);
+  }
+
+  // Stop thread
+  if (magnet_switch.readPins() && magnet_switch.readPins())
+  {
+    stop_flag = true;
   }
 }
 
